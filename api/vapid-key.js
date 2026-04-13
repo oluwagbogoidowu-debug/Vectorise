71bcf50257b3cf1749492f9872bdcd7323aaeb96
Oluwagbogo <oluwagbogoidowu@gmail.com>
2026-04-13 11:43:37 +0100

    Update public key in vapid-key handler

--- api/vapid-key.js
+++ api/vapid-key.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ publicKey: "test-ok" });
+  res.status(200).json({ publicKey: "BPG4lkECd-HiF4W8WPANEjk6QswjHOFk4fnvdTceYYu_L4ORxw7PogMDAqANoL1DnPM0L27zEpqM6Zokn7ZJhdc" });
 }
