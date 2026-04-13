8463c02ce1bfe82abb08118a01dd7e90f4845168
Oluwagbogo <oluwagbogoidowu@gmail.com>
2026-04-13 11:06:30 +0100

    Create webpush.js with content from webpush.ts

---
+++ utils/webpush.js
@@ -0,0 +1,6 @@
+// This is an example content copied from webpush.ts
+// Make sure to replace it with the actual content.
+
+export function sendPushNotification() {
+    // Push notification logic here
+}
